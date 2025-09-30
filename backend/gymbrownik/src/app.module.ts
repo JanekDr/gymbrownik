c64522718645fb520689bcc7b172bc088fc72325
Jan Druszcz <jan.druszcz@onet.eu>
2025-09-30 14:40:36 +0200

    feat: add schema of models

--- backend/gymbrownik/src/app.module.ts
+++ backend/gymbrownik/src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { UserModule } from './user/user.module';
+import { TrainingModule } from './training/training.module';
+import { TrainingPlanModule } from './training-plan/training-plan.module';
+import { WorkoutModule } from './workout/workout.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, UserModule, TrainingModule, TrainingPlanModule, WorkoutModule],
   controllers: [AppController],
   providers: [AppService],
 })
